e079315117a11b46d0facf4101e345496808a878
Srdjan Pajic <s.pajic@diamonddogs.cc>
2015-09-30 14:07:27 +0200

    updated employee service to use $resource

--- Insight/src/Insight/app/employeesService.js
+++ Insight/src/Insight/app/employeesService.js
@@ -7,18 +7,9 @@ angular
 function Employees($resource, $http) {
 
     var employeesServicesObj = {};
-    //return $resource('/api/employees/:id');
+    var employees = $resource('/api/employees/:id');
     employeesServicesObj.getAll = function () {
-        return [{ firstName: 'test' }];
-        //$http({
-        //    url: '/api/employees/',
-        //    method: 'GET',
-        //    params: []
-        //}).success(function (result) {
-        //    return result;
-        //}).error(function (err) {
-        //    console.log(err);
-        //});
+        return employees.query();
     }
 
     return employeesServicesObj;
